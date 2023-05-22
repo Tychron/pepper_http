2a9aeac7c1b91ef2a75b3d1b62d517784af9692a
Corey Powell <mistdragon100@gmail.com>
2023-05-22 16:02:58 -0500

    Bumped version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pepper.HTTP.MixProject do
   def project do
     [
       app: :pepper_http,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       elixirc_options: [
